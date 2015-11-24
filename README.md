cf2ef6068e76909f2535e05374130ecc1e5167b7
Arthur Wolf <wolf.arthur@gmail.com>
2015-11-24 18:59:34 +0100

    added the basic compile script

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Octofab
 A tool to turn the awesome Octoprint into a standalone independent application for use on Smoothieboards
+
+# Using
+
+First clone this repository :
+
+git clone https://github.com/arthurwolf/Octofab.git
+
+Move to the new repository
+
+cd Octofab/
+
+In there, clone Octoprint itself
+
+git clone https://github.com/foosel/OctoPrint.git
+
+Move to the OctoPrint folder: 
+
+cd OctoPrint
+
+Create a user-owned virtual environment therein: 
+
+virtualenv --system-site-packages venv
+
+Install OctoPrint into that virtual environment: 
+
+./venv/bin/python setup.py install
+
+Run Octoprint
+
+./venv/bin/octoprint
+
+
+
---
+++ compile.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+use strict;
+use WWW::Mechanize;
+use HTML::TreeBuilder;
+use Slurp;
+
+# Step 0, set context
+my $octoprint = "http://localhost:5000";
+
+# Step 1, get the octoprint page
+
+my $mech = WWW::Mechanize->new();
+$mech->get( $octoprint );
+
+# Step 2, build a tree
+
+my $root = HTML::TreeBuilder->new_from_content( $mech->content );
+
+# Step 3, find and include all scripts
+
+for my $script ( $root->look_down( _tag => "script", src => qr/static/ ) ){
+    # Get the filename 
+    my $src = $script->attr("src");
+    
+    # Remove the src attribute 
+    $script->attr( src => undef );
+
+    # Get the remote content
+    $mech->get( $octoprint . $src );
+
+    # Insert the content into the tag
+    $script->push_content( $mech->content );
+
+}
+
+# Step 4, find and include all the stylesheets
+
+for my $style ( $root->look_down( _tag => "link", href => qr/webassets/ )){
+    # Get the filename 
+    my $src = $style->attr("href");
+   
+    # Remove the href attribute
+    $style->attr( href => undef );
+
+    # Change the tag
+    $style->tag("style");
+
+    # Get the remote content
+    $mech->get( $octoprint . $src );
+
+    # Insert the content into the tag
+    $style->push_content( $mech->content );
+
+}
+
+# Step 5, insert custom scripts
+
+for my $jsfile ( split("\n", `ls src/js/*`) ){
+    # Get the file's content
+    my $content = slurp($jsfile);
+
+    # Make the new tag
+    my $tag = HTML::Element->new('script', type => "text/javascript");
+    $tag->push_content($content);
+
+    # Find the body
+    my $body = [$root->look_down( _tag => 'body' )]->[0];
+    
+    # Add to the end of the body
+    $body->push_content($tag);
+}
+
+# Final step, dump the whole file
+print $root->as_HTML();
+
+
+
+
+
