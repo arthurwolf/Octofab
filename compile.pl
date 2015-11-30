58524281a9a0eccb5e93832a451c96abc1de0eea
Arthur Wolf <wolf.arthur@gmail.com>
2015-11-30 20:32:40 +0100

    getting a file list now works

--- compile.pl
+++ compile.pl
@@ -3,6 +3,7 @@ use strict;
 use WWW::Mechanize;
 use HTML::TreeBuilder;
 use File::Slurp;
+use utf8;
 
 # Step 0, set context
 my $octoprint = "http://localhost:5000";
@@ -74,88 +75,15 @@ for my $may_be_a_script ( @lines ){
     }
 }
 
-print join("\n", @scripts_inlined);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-exit;
-# Step 2, build a tree
-
-my $root = HTML::TreeBuilder->new_from_content( $mech->content );
-
-# Step 3, find and include all scripts
-
-for my $script ( $root->look_down( _tag => "script", src => qr/static/ ) ){
-    # Get the filename 
-    my $src = $script->attr("src");
-    
-    # Remove the src attribute 
-    $script->attr( src => undef );
-
-    # Get the remote content
-    $mech->get( $octoprint . $src );
-
-    # Insert the content into the tag
-    $script->push_content( $mech->content );
-
-}
-
-# Step 4, find and include all the stylesheets
-
-for my $style ( $root->look_down( _tag => "link", href => qr/webassets/ )){
-    # Get the filename 
-    my $src = $style->attr("href");
- 
-    # Remove the href attribute
-    $style->attr( href => undef );
-
-    # Change the tag
-    $style->tag("style");
-
-    # Get the remote content
-    $mech->get( $octoprint . $src );
-
-    # Insert the content into the tag
-    $style->push_content( $mech->content );
-
-}
-
-# Step 5, insert custom scripts
-
-for my $jsfile ( split("\n", `ls src/js/*`) ){
-    # Get the file's content
-    my $content = read_file($jsfile);
-
-    # Make the new tag
-    my $tag = HTML::Element->new('script', type => "text/javascript");
-    $tag->push_content($content);
-
-    # Find the body
-    my $body = [$root->look_down( _tag => 'body' )]->[0];
-    
-    # Add to the end of the body
-    $body->push_content($tag);
-}
-
 # Final step, dump the whole file
-#write_file('test.html', $root->as_HTML());
-print $root->as_HTML('', '    '), "\n";
+open( my $fh, '>', 'index.html' );
+binmode $fh, ':encoding(UTF-8)';
+print $fh join("\n", @scripts_inlined);
+close $fh;
 
-# Tidy the file
-#system("tidy -i -utf8 -q -m test.html");
+system("python smoothie-upload.py index.html 192.168.0.18");
+
+
 
 
 
--- src/js/dataupdater.js
+++ src/js/dataupdater.js
@@ -1,27 +1,26 @@
 var global_sockjs;
-
+var smoothie;
 
 var blob = new Blob([document.querySelector('#inlineworker').textContent]);
 GCODE_WORKER = window.URL.createObjectURL(blob);
 
 function SockJS(uri, what, options){
     global_sockjs = this;
-    console.log(this);
 
     this.on_ajax_connect = function(){
         var _onmessage = this.onmessage;
-        var callback = function (data) { _onmessage(data); }; 
+        this.callback = function (data) { _onmessage(data); }; 
         
-        console.log("false sockjs calls"); 
-        callback.call(undefined, {"type":"message","data":{"connected":{"display_version":"1.2.7 (master branch)","apikey":"A6977F02830F4154AD3B123F3A67FCB1","version":"1.2.7","branch":"master","plugin_hash":"f88be250e0099eeecbf10a90bb32fa4b"}}});
+        //console.log("false sockjs calls"); 
+        //this.callback.call(undefined, {"type":"message","data":{"connected":{"display_version":"1.2.7 (master branch)","apikey":"A6977F02830F4154AD3B123F3A67FCB1","version":"1.2.7","branch":"master","plugin_hash":"f88be250e0099eeecbf10a90bb32fa4b"}}});
 
-        callback.call(undefined,{"type":"message","data":{"history":{"logs":[],"offsets":{},"serverTime":1448466981.314448,"messages":[],"state":{"text":"Offline","flags":{"operational":false,"paused":false,"printing":false,"sdReady":false,"error":false,"ready":false,"closedOrError":true}},"temps":[],"job":{"estimatedPrintTime":null,"filament":{"volume":null,"length":null},"file":{"origin":null,"date":null,"name":null,"size":null},"lastPrintTime":null},"currentZ":null,"progress":{"completion":null,"printTime":null,"filepos":null,"printTimeLeft":null}}}}); 
+        //this.callback.call(undefined,{"type":"message","data":{"history":{"logs":[],"offsets":{},"serverTime":1448466981.314448,"messages":[],"state":{"text":"Offline","flags":{"operational":false,"paused":false,"printing":false,"sdReady":false,"error":false,"ready":false,"closedOrError":true}},"temps":[],"job":{"estimatedPrintTime":null,"filament":{"volume":null,"length":null},"file":{"origin":null,"date":null,"name":null,"size":null},"lastPrintTime":null},"currentZ":null,"progress":{"completion":null,"printTime":null,"filepos":null,"printTimeLeft":null}}}}); 
 
-        callback.call(undefined,{"type":"message","data":{"event":{"type":"ClientOpened","payload":{"remoteAddress":"127.0.0.1"}}}});
+        //this.callback.call(undefined,{"type":"message","data":{"event":{"type":"ClientOpened","payload":{"remoteAddress":"127.0.0.1"}}}});
 
-        callback.call(undefined,{"type":"message","data":{"timelapse":null}});
+        //this.callback.call(undefined,{"type":"message","data":{"timelapse":null}});
 
-        callback.call(undefined,{"type":"message","data":{"current":{"logs":["Send: M105"],"offsets":{},"serverTime":1448892107.575042,"busyFiles":[],"messages":[],"job":{"file":{"origin":null,"date":null,"name":null,"size":null},"estimatedPrintTime":null,"averagePrintTime":null,"filament":null,"lastPrintTime":null},"temps":[],"state":{"text":"Operational","flags":{"operational":true,"paused":false,"printing":false,"sdReady":true,"error":false,"ready":true,"closedOrError":false}},"currentZ":null,"progress":{"completion":0,"printTimeLeft":null,"printTime":null,"filepos":null}}}});
+        //this.callback.call(undefined,{"type":"message","data":{"current":{"logs":["Send: M105"],"offsets":{},"serverTime":1448892107.575042,"busyFiles":[],"messages":[],"job":{"file":{"origin":null,"date":null,"name":null,"size":null},"estimatedPrintTime":null,"averagePrintTime":null,"filament":null,"lastPrintTime":null},"temps":[],"state":{"text":"Operational","flags":{"operational":true,"paused":false,"printing":false,"sdReady":true,"error":false,"ready":true,"closedOrError":false}},"currentZ":null,"progress":{"completion":0,"printTimeLeft":null,"printTime":null,"filepos":null}}}});
 
     }; 
 
@@ -33,8 +32,7 @@ $.ajaxPrefilter(function( options, originalOptions, jqXHR ){
     // If this is an API call
     if( options.url.substr(0,5) == "/api/" || options.url.substr(0,8) == "/sockjs/" ){
         handleAjaxCall(options, originalOptions, jqXHR);    
-        options = {};
-        originalOptions = {};
+        jqXHR.abort(); 
     }else{
         console.log("else:");
         console.log(options);
@@ -47,6 +45,10 @@ function handleAjaxCall(options, originalOptions, jqXHR){
     console.log(options);
 
     var answer = '';
+
+    var originalSuccess = originalOptions.success || options.success;
+    var next = function (data) { originalSuccess(data);
+    }; 
 
     switch( options.url ){
         case '/api/printerprofiles':
@@ -72,8 +74,8 @@ function handleAjaxCall(options, originalOptions, jqXHR){
         case '/api/printer/commond/custom':
             answer = {"controls": []};
         case '/api/files':
-            answer = {"files": [{"date": 1448398410, "gcodeAnalysis": {"estimatedPrintTime": 5663.590175169759, "filament": {"tool0": {"length": 4142.776639999936, "volume": 26.42841513801609}}}, "hash": "9fee0176f347fd00ede5788a5f4b904020dead49", "links": [], "name": "helice3.gcode", "origin": "local", "refs": {"download": "http://localhost:5000/downloads/files/local/helice3.gcode", "resource": "http://localhost:5000/api/files/local/helice3.gcode"}, "size": 5690538, "type": "machinecode"}], "free": 413874028544, "total": 1871855992832};
-            break;
+            smoothie.get_file_list(next);
+            return;
         case '/api/slicing/cura/profiles':
             answer = {};
             break;
@@ -83,14 +85,64 @@ function handleAjaxCall(options, originalOptions, jqXHR){
         case '/api/logs':
             answer ={"files": [{"date": 1448405215, "name": "octoprint.log", "refs": {"download": "http://localhost:5000/downloads/logs/octoprint.log", "resource": "http://localhost:5000/api/logs/octoprint.log"}, "size": 22993}, {"date": 1448312671, "name": "plugin_cura_engine.log", "refs": {"download": "http://localhost:5000/downloads/logs/plugin_cura_engine.log", "resource": "http://localhost:5000/api/logs/plugin_cura_engine.log"}, "size": 0}, {"date": 1448312670, "name": "serial.log", "refs": {"download": "http://localhost:5000/downloads/logs/serial.log", "resource": "http://localhost:5000/api/logs/serial.log"}, "size": 0}, {"date": 1448312671, "name": "plugin_pluginmanager_console.log", "refs": {"download": "http://localhost:5000/downloads/logs/plugin_pluginmanager_console.log", "resource": "http://localhost:5000/api/logs/plugin_pluginmanager_console.log"}, "size": 0}], "free": 413871190016, "total": 1871855992832};
             break;
+        case '/api/login':
+            // TODO : proper answer
+            //smoothie.initialize();
     }
 
     if( answer == '' ){ return; }
 
-    var originalSuccess = originalOptions.success || options.success;
-    var next = function (data) { originalSuccess(data);
-    }; 
-    next.call(undefined, answer);
+   next.call(undefined, answer);
 
 }
+
+function Smoothie(){
+
+    this.initialized = false;
+    this.file_list = [];
+
+    this.initialize = function(){
+        if( !this.initialized ){
+            this.initialized = true; 
+            console.log("initializing");
+            this.on_page_loaded(); 
+        }
+    };
+
+    this.on_page_loaded = function(){
+        console.log("page loaded, trying to contact smoothie");
+    };
+
+    this.get_file_list = function(success_function){
+        var that = this;
+        $.post('/command', "ls -s /sd/\n").done(function(data){
+            that.file_list = []; 
+            data.split("\n").map(function(item){
+                if( item == '' ){ return; }
+                var name = item.split(' ')[0];
+                var size = item.split(' ')[1];
+                that.file_list.push({
+                    "date":             1448398410,
+                    "gcodeAnalysis":    {"estimatedPrintTime":0, "filament":{ "tool0":{ "length":0, "volume":0 } } },
+                    "hash":             "no hash",
+                    "links":            [],
+                    "name":             name,
+                    "origin":           "local",
+                    "refs":             { "download":"/sd/" + name, "resource":"/sd/" + name },
+                    "size":             size,
+                    "type":             "machinecode"
+                });
+            });
+            answer = {"files": smoothie.file_list, "free": 0, "total": 0};
+            success_function.call(undefined, answer);
+            console.log(answer);
+        });
+    };
+
+}
+
+smoothie = new Smoothie();
+smoothie.initialize();
+
+
 
